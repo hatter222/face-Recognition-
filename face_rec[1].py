f7d669a4a10ce09b3d334a293f034e947957273a
unknown <asadullah.dal17@gmail.com>
2024-05-14 23:15:31 +0500

    update file

--- face_rec[1].py
+++ face_rec[1].py
@@ -1,28 +1,29 @@
 import face_recognition
 import os
 import cv2
+
 # import requests
 import numpy as np
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture("../Raza_sajal.mp4")
 
-Face_IDs = 'Face_IDs'
+Face_IDs = "Face_IDs"
 TOLERANCE = 0.6
 FRAME_THICKNESS = 3
 FONT_THICKNESS = 2
 # default: 'hog', other one can be 'cnn' - CUDA accelerated (if available) deep-learning pretrained model
-MODEL = 'hog'
+MODEL = "hog"
 
 
 # Returns (R, G, B) from name
 def name_to_color(name):
     # Take 3 first letters, tolower()
     # lowercased character ord() value rage is 97 to 122, substract 97, multiply by 8
-    color = [(ord(c.lower())-97)*8 for c in name[:3]]
+    color = [(ord(c.lower()) - 97) * 8 for c in name[:3]]
     return color
 
 
-print('Loading known faces...')
+print("Loading known faces...")
 known_faces = []
 known_names = []
 print(Face_IDs)
@@ -31,12 +32,12 @@ print(Face_IDs)
 for name in os.listdir(Face_IDs):
 
     # Next we load every file of faces of known person
-    for filename in os.listdir(f'{Face_IDs}/{name}'):
+    for filename in os.listdir(f"{Face_IDs}/{name}"):
 
         # Load an image
-        image = face_recognition.load_image_file(
-            f'{Face_IDs}/{name}/{filename}')
-
+        # image = face_recognition.load_image_file(f"{Face_IDs}/{name}/{filename}")
+        image = cv2.imread(f"{Face_IDs}/{name}/{filename}")
+        print(image.shape)
         # Get 128-dimension face encoding
         # Always returns a list of found faces, for this purpose we take first face only (assuming one face per image as you can't be twice on one image)
         encoding = face_recognition.face_encodings(image)[0]
@@ -46,7 +47,7 @@ for name in os.listdir(Face_IDs):
         known_names.append(name)
 
 
-print('Processing unknown faces...')
+print("Processing unknown faces...")
 # Now let's loop over a folder of faces we want to label
 # cap= cv2.VideoCapture(0)
 while True:
@@ -58,7 +59,7 @@ while True:
     # Load image
     # print(f'Filename {filename}', end='')
     # frame = face_recognition.load_image_file(frame)
-# ////////////////////////////////////
+    # ////////////////////////////////////
     # This time we first grab face locations - we'll need them to draw boxes
     locations = face_recognition.face_locations(frame, model=MODEL)
 
@@ -76,15 +77,16 @@ while True:
 
         # We use compare_faces (but might use face_distance as well)
         # Returns array of True/False values in order of passed known_faces
-        results = face_recognition.compare_faces(
-            known_faces, face_encoding, TOLERANCE)
+        results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
 
         # Since order is being preserved, we check if any face was found then grab index
         # then label (name) of first matching known face withing a tolerance
         match = None
-        if True in results:  # If at least one is true, get a name of first of found labels
+        if (
+            True in results
+        ):  # If at least one is true, get a name of first of found labels
             match = known_names[results.index(True)]
-            print(f' - {match} from {results}')
+            print(f" - {match} from {results}")
 
             # Each location contains positions in order: top, right, bottom, left
             top_left = (face_location[3], face_location[0])
@@ -94,8 +96,7 @@ while True:
             color = name_to_color(match)
 
             # Paint frame
-            cv2.rectangle(frame, top_left, bottom_right,
-                          color, FRAME_THICKNESS)
+            cv2.rectangle(frame, top_left, bottom_right, color, FRAME_THICKNESS)
 
             # Now we need smaller, filled grame below for a name
             # This time we use bottom in both corners - to start from bottom and move 50 pixels down
@@ -106,12 +107,19 @@ while True:
             cv2.rectangle(frame, top_left, bottom_right, color, cv2.FILLED)
 
             # Wite a name
-            cv2.putText(frame, match, (face_location[3] + 10, face_location[2] + 15),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), FONT_THICKNESS)
+            cv2.putText(
+                frame,
+                match,
+                (face_location[3] + 10, face_location[2] + 15),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (200, 200, 200),
+                FONT_THICKNESS,
+            )
 
     # Show image
     cv2.namedWindow("filename", cv2.WINDOW_FREERATIO)
     cv2.imshow("filename", frame)
-    if cv2.waitKey(1) == ord('q'):
+    if cv2.waitKey(1) == ord("q"):
         break
-    cv2.destroyWindow(filename)
+cv2.destroyAllWindows()
